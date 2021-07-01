99fde53cee148b3dca4ed0999ac75dd10ccffd14
Srigowri Deogirikar <32318383+srigowrideo@users.noreply.github.com>
2021-07-01 19:34:22 +0530

    Add files via upload

---
+++ boolcancel.java
@@ -0,0 +1,17 @@
+package fibonacciseries;
+
+public class BoolDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+boolean b=false;
+System.out.println("b is"+b);
+b=true;System.out.println("b is"+b);
+if(b)System.out.println("True block");
+b=false;
+if(b) System.out.println("False aBlock will not be executed");
+b=(3<5);
+System.out.println("3<5 is"+b);
+	}
+	}
+
